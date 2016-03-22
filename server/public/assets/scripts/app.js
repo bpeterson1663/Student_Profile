5b2d9607c9ae7a60277cf6b9077079300c337d46
bpeterson1663 <bpeterson1663@gmail.com>
2016-03-22 14:05:49 -0500

    First Commit

---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,18 @@
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("MainController", ["$scope","$http", function($scope, $http){
+
+  $scope.getStudentData = function(){
+    $http.get("/kappaStudents").then(function(response){
+      
+      $scope.studentArray = response.data.students;
+      console.log($scope.studentArray);
+    });
+
+    console.log("Function being executed");
+  };
+
+  $scope.getStudentData();
+
+}]);
